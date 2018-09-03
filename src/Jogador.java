271d5fc3c51294c856b98c45520d448a2191a2c8
Rick <rickbatera2012@gmail.com>
2018-09-03 15:29:27 -0300

    Classe Jogador
    Classe feita por Johnny Lima.
    
    -Constructor, Get, e Set da variável "apelido".

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,2 +1,21 @@
 public class Jogador {
+
+    private String apelido;
+    private money;
+
+    //Constructor: jogador recebe um apelido.
+    public jogador(String apelido){
+        this.apelido = apelido;
+    }
+
+
+    //Método Get Apelido
+    public String getApelido() {
+        return apelido;
+    }
+
+    //Método Set Apelido
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
 }
