06a06820bd8d129058e679cf93884d0a6ad01073
julio cesar <senhorodrigues@live.com>
2018-09-04 09:13:59 -0300

    update

--- src/Baralho.java
+++ src/Baralho.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
+import java.util.Random;
+
  public class Baralho {
+	 
     private ArrayList<Carta> baralho;
     private String[] Naipes = {"P","O","C","E"}; //Paus, Ouro, Copa, Espadas
     private String[] Tipos = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    
      public Baralho(){
         baralho = new ArrayList<Carta>();
         for (String naipe : Naipes ) {
@@ -11,8 +15,24 @@ import java.util.ArrayList;
             }
         }
     }
-     public void Embaralhar(){
-     }
-     public void MostrarBaralho(){
-     }
+    
+    public void Embaralhar(){
+    	ArrayList<Carta> NovoBaralho = new ArrayList<Carta>();
+    	Random aleatorio = new Random();
+    	int tamanho = baralho.size();
+    	
+    	//For para embaralhar
+    	for(int i = 0; i < tamanho; i++) {
+    		
+    	}
+    	
+    	//Baralho atual recebe o baralho embaralhado
+    	baralho = NovoBaralho;
+    }
+    
+    public void MostrarBaralho(){
+    	for(Carta carta : baralho) {
+    		carta.Mostrar();
+    	}
+    }
 }
--- src/Carta.java
+++ src/Carta.java
@@ -1,16 +1,48 @@
 public class Carta {
-    private String Naipe;
+	
+	private String Naipe;
     private String Tipo;
     private int Valor;
-     public Carta(String naipe, String tipo){
+    
+	public Carta(String naipe, String tipo){
+    	 
         Naipe = naipe;
         Tipo = tipo;
         if(Tipo.equals("A")){
-            Valor = 1;
+            Valor = 1; // especificar na hora de chamar se vale 1 ou 11
         } else if (Tipo.equals("J") || Tipo.equals("Q") || Tipo.equals("K")){
             Valor = 10;
         } else {
             Valor = Integer.parseInt(tipo);
         }
-    }
+	}
+    
+	public void Mostrar() {
+		String naipe;
+		String tipo;
+		
+		if(Naipe.equals("P"))
+			naipe = "Paus";
+		else if(Naipe.equals("O"))
+			naipe = "Ouro";
+		else if(Naipe.equals("C"))
+			naipe = "Copas";
+		else
+			naipe = "Espadas";
+		
+		if(Tipo.equals("A"))
+			tipo = "Ás";
+		else if(Tipo.equals("J"))
+			tipo = "Valete";
+		else if(Tipo.equals("Q"))
+			tipo = "Dama";
+		else if(Tipo.equals("K"))
+			tipo = "Rei";
+		else
+			tipo = Tipo;
+		
+		System.out.println(tipo + " De " + naipe);
+			
+	}
+	
 }
