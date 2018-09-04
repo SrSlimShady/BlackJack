0d8eecc91c873a25bdfaaabcb2030736ac043c29
Rick <rickbatera2012@gmail.com>
2018-09-04 17:00:12 -0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//		Jogo BJ = new Jogo();
+		
+		//SAÍDA DE TELA
+		
+		System.out.println("as");
+		
+		
+		System.out.println("┌─────────┐");
+		System.out.println("│A        │");
+		System.out.println("│         │");
+		System.out.println("│    ♣    │");
+		System.out.println("│         │");
+		System.out.println("│        A│");
+		System.out.println("└─────────┘");	
+		
+		
 
 	}
 
