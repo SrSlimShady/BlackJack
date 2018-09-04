5a6a9dbaece407bea1e01ce2859eda3acca243c3
Rick <rickbatera2012@gmail.com>
2018-09-04 15:52:17 -0300

    Refresh na Classe

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,51 +1,60 @@
 import java.util.ArrayList;
- public class Jogador {
-     private String apelido;
-    private money;
+
+public class Jogador {
+
+    private String apelido;
     private double money = 100;
     private ArrayList<Carta> cartaJogador;
-     //Constructor: jogador recebe um apelido.
-    public jogador(String apelido){
-    public jogador(String apelido, double money){
+
+    //Constructor: jogador recebe um apelido.
+    public Jogador(String apelido, double money){
         this.apelido = apelido;
         this.money = money;
         cartaJogador = new ArrayList<>();
     }
-     //Método Get money
+
+    //Metodo Get money
     public double getMoney() {
         return money;
     }
-     //Método para Setar o money
+
+    //Metodo para Setar o money
     /*
-    * Método Setter do Dinheiro.
+    * Metodo Setter do Dinheiro.
     * @<b>Sitaxe:</b><i>Srint setMoney (double money, boolean option)</i>
     * @money : é o dinheiro a ser usado.
     * @option : true or 1 = adicionar dinheiro; false, 0 or default = retirar dinheiro;
     *@autor Johnny Lima
     */
-    public void setMoney(double money, boolean option ) {
-        private err ;
+    public void setMoney(double money, int option ) {
         switch (option) {
-            case true: //Adicionar money
+            case 1: //Adicionar money
                     this.money += money;
                 break;
-             case false: //retirar money
+
+            case 0: //retirar money
                     this.money -= money;
                 break;
-             default: //retirar money
+
+            default: //retirar money
                     this.money -= money;
                 break;
         }
     }
-     public void addCartaJogador(Carta carta){
+
+
+    public void addCartaJogador(Carta carta){
         cartaJogador.add(carta);
     }
-     //Método Get Apelido
+
+    //Método Get Apelido
     public String getApelido() {
         return apelido;
-     }
-     //Método Set Apelido
-     public void setApelido(String apelido) {
-         this.apelido = apelido;
-     }
- }
+
+    }
+
+    //Método Set Apelido
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+}
