ddefe010f491116057f9f2f83357dd91738b558f
Rick <rickbatera2012@gmail.com>
2018-09-05 02:02:51 -0300

    Criação da main com os controles de tela

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.function.Function;
+
+
 
 public class Main {
 
@@ -7,21 +10,84 @@ public class Main {
 		
 //		Jogo BJ = new Jogo();
 		
-		//SAÍDA DE TELA
 		
-		System.out.println("as");
+			
+		
+		//MOLDE
+		int moldeOpt = 1;
+		int tm = 79;
+		
+		//MENU
+		int optMolde = 0; //0 = simples ou 1 = duplo
+		int tmMolde = 20;
+		
+		Molde mol = new Molde();
 		
 		
-		System.out.println("┌─────────┐");
-		System.out.println("│A        │");
-		System.out.println("│         │");
-		System.out.println("│    ♣    │");
-		System.out.println("│         │");
-		System.out.println("│        A│");
-		System.out.println("└─────────┘");	
+		Scanner entrada = new Scanner(System.in);
+		int opcao, temMolde=0;
+		String msg="", head="";
+		
+		//COMEÇO
+		do {
+			
+			mol.Titulo();
+			if (temMolde==0) {
+				
+				System.out.printf("\n%s\n",msg);
+				//MENU
+				mol.Molde("1 - Novo Jogo"		,optMolde,tmMolde);
+				mol.Molde("99 - Sair"		,optMolde,tmMolde);
+				System.out.printf("\nDigite a opcao desejada para 'Lista': ");
+				for (int i = 0; i < 21; ++i) System.out.println();
+				
+				//fazendo a leitura da opção
+				
+				
+				
+			}
+			else {
+				
+				mol.Molde(head, moldeOpt, tm);
+				head="";
+				temMolde=0;
+				for (int i = 0; i < 27; ++i) System.out.println();
+				
+				
+				
+			}
+			
+			opcao = entrada.nextInt();
+			switch (opcao) {
+				case 1: // criar
+					head="Novo Jogo";
+					temMolde=1;
+					for (int i = 0; i < 50; ++i) System.out.println();
+					//Chamar classe
+					break;
+				case 99: //sair
+//					for (int i = 0; i < 17; ++i) System.out.println();
+					mol.Titulo();
+					mol.Molde("Sair", moldeOpt, tm);
+					System.out.printf("\n\nVocê saiu do Jogo.");
+					for (int i = 0; i < 25; ++i) System.out.println();
+					break;
+				default: // opcao invalida
+					msg = "Opcao Invalida!";
+					for (int i = 0; i < 50; ++i) System.out.println();
+			}
+		} while (opcao != 99); // opcao de saida
+		
+		
+		
 		
 		
 
+		
+
 	}
+	
+	
+	
 
 }
